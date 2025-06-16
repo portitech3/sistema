bb99b0d4cd083e4d085b1ea9f2484c94c62a9960
portillojesus <jesusportillo000018@gmail.com>
2025-06-16 12:13:13 -0300

    Se agrego el detalle de pedidos, la opcion de editar y la opcion de eliminar

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('nuevo/', views.crear_pedido, name='crear_pedido'),
     path('', views.lista_pedidos, name='lista_pedidos'),  # si tenés una vista de listado
+    path('<int:pedido_id>/', views.detalle_pedido, name='detalle_pedido'),
+    path('editar/<int:pedido_id>/', views.editar_pedido, name='editar_pedido'),
+    path('eliminar/<int:pedido_id>/', views.eliminar_pedido, name='eliminar_pedido'),
+
 ]
--- pedidos/views.py
+++ pedidos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import PedidoForm
 from .models import Pedido
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 def crear_pedido(request):
     if request.method == 'POST':
@@ -15,3 +17,39 @@ def crear_pedido(request):
 def lista_pedidos(request):
     pedidos = Pedido.objects.all()
     return render(request, 'pedidos/lista_pedidos.html', {'pedidos': pedidos})
+
+
+
+def detalle_pedido(request, pedido_id):
+    pedido = get_object_or_404(Pedido, id=pedido_id)
+    return render(request, 'pedidos/detalle_pedido.html', {'pedido': pedido})
+
+
+def editar_pedido(request, pedido_id):
+    pedido = get_object_or_404(Pedido, id=pedido_id)
+
+    if request.method == 'POST':
+        form = PedidoForm(request.POST, instance=pedido)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'El pedido ha sido actualizado correctamente.')
+            return redirect('lista_pedidos')
+    else:
+        form = PedidoForm(instance=pedido)
+
+    return render(request, 'pedidos/editar_pedido.html', {
+        'form': form,
+        'pedido': pedido
+    })
+
+def eliminar_pedido(request, pedido_id):
+    pedido = get_object_or_404(Pedido, id=pedido_id)
+
+    if request.method == 'POST':
+        pedido.delete()
+        messages.success(request, f'El pedido N°{pedido.id} ha sido eliminado.')
+        return redirect('lista_pedidos')
+
+    return render(request, 'pedidos/eliminar_pedido.html', {
+        'pedido': pedido
+    })
